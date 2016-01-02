a901195efb5ec348dbfdea1b0215c757dee0aa2a
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-01-02 18:55:05 +0100

    wait until other view unbinds before rendering

--- public/js/main_dev.js
+++ public/js/main_dev.js
@@ -1,15 +1,18 @@
-define(['jquery', 'app', 'marionette', 'backbone', './phantom.template.view', './phantom.template.model'],
+define(['jquery', 'app', 'marionette', 'backbone', './phantom.image.template.view', './phantom.image.template.model'],
   function ($, app, Marionette, Backbone, TemplateView, Model) {
     app.on('template-extensions-render', function (context) {
       var view
 
       function renderRecipeMenu () {
         if (context.template.get('recipe') === 'phantom-image') {
+          console.log('here');
           var model = new Model()
           model.setTemplate(context.template)
           view = new TemplateView({model: model})
 
-          context.extensionsRegion.show(view, 'phantomImage')
+          setTimeout(function() {
+            context.extensionsRegion.show(view, "phantomImage");
+          }, 0);
         } else {
           if (view != null) {
             $(view.el).remove()
